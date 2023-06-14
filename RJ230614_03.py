d93bcb4dc9fce7db12a323f87a55347a2ecb7aef
Kohki-Fukuoka <117794511+Kohki-Fukuoka@users.noreply.github.com>
2023-06-14 16:43:45 +0900

    2023/6/14 課題4
    sample.zipファイルには，kitamura_00000_kug.txtからkitamura_00999_kug.txtまでの1000個のファイルが含まれており，それぞれに整数が書かれている．ファイル名の数字が奇数のものだけに関して，それらのファイルに書かれた数字の合計を求めよ．
    txtファイル群のディレクトリは 0614/sample/sample 下

---
+++ RJ230614_03.py
@@ -0,0 +1,37 @@
+
+def read_files():
+
+    file_path_before = "0614/sample/sample/kitamura_"
+    file_path_after = "_kgu.txt"
+
+    sum = 0
+
+    for i in range(1, 1000, 2):
+        file_path = f"{file_path_before}{i:05}{file_path_after}"
+        with open(file_path, "r") as f:
+            line_sum = calc_file(f)
+            sum += line_sum
+    
+    return sum
+
+
+def calc_file(f):
+    contents = f.read()
+    lines = contents.split("\n")
+
+    sum = 0
+
+    for line in lines:
+        try:
+            val = int(line)
+            sum += val
+        except:
+            pass
+    
+    return sum
+
+def main():
+    print(read_files())
+
+if __name__ == "__main__":
+    main()
